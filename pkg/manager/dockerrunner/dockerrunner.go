7c359efb6c48b377267ea5a0e204e84f169daa85
Hansika123 <hansijw76@gmail.com>
2020-07-25 13:34:01 +0530

    add containerrunner

---
+++ pkg/manager/dockerrunner/dockerrunner.go
@@ -0,0 +1,36 @@
+package dockerrunner
+
+import (
+	docker "github.com/fsouza/go-dockerclient"
+	"log"
+)
+
+type ResponseObj struct {
+	ResponseCode int
+	ResponseDesc string
+}
+
+func ContainerRunner(containerId string) (ResponseObj, error) {
+	var responseObj ResponseObj
+	log.Println("Starting container Runner : containerId: " + containerId)
+	client, err := docker.NewClientFromEnv()
+	if err != nil {
+		responseObj.ResponseCode = 1
+		responseObj.ResponseDesc = "Error Running Docker Container Id: " + containerId
+		return responseObj, err
+	}
+
+	err = client.StartContainer(containerId, &docker.HostConfig{
+		PublishAllPorts: true,
+	})
+	if err != nil {
+		log.Println(err)
+		responseObj.ResponseCode = 1
+		responseObj.ResponseDesc = "Successfully Run failure :" + containerId
+	} else {
+		responseObj.ResponseCode = 0
+		responseObj.ResponseDesc = "Successfully Run The Container :" + containerId
+	}
+
+	return responseObj, err
+}
