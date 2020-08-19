571274c6305d3a1e8afa42e8198c6d6c39e2cf33
Hansika123 <hansijw76@gmail.com>
2020-08-19 23:47:05 +0530

    add cache updater

--- main.go
+++ main.go
@@ -3,15 +3,25 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/patrickmn/go-cache"
+	"log"
+	"os"
 	"time"
-	"ukiyo/api/historyapilayer"
 	"ukiyo/api/registryapilayer"
 	"ukiyo/api/schedulerapilayer"
+	"ukiyo/pkg/process"
 	"ukiyo/pkg/scheduler/eventsheduler"
 	"ukiyo/pkg/webhook-listener"
 )
 
 func main() {
+
+	file, err := os.OpenFile("info.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	log.SetOutput(file)
+
 	r := gin.Default()
 	c := cache.New(1*time.Minute, 1*time.Minute)
 	webhook_listener.HealthCheck(r)
@@ -22,8 +32,7 @@ func main() {
 	schedulerapilayer.SaveRepositoryScheduledTime(r, c)
 	schedulerapilayer.EditRepositoryScheduledTime(r, c)
 	schedulerapilayer.DeleteRepositoryScheduledTime(r, c)
-	historyapilayer.GetAllContainerHistory(r)
-	historyapilayer.GetAllContainerHistoryByName(r)
 	eventsheduler.ScheduledRunner("run", "", 0, c)
+	go process.Process(c)
 	r.Run(":8080")
 }
